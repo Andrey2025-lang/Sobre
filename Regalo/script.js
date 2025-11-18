ecae4522b16c475776a53133686899125ea390dd
Andrey2025-lang <andrey.gonzalez.villegas@est.una.ac.cr>
2025-11-17 23:15:32 -0600

    Delete Regalo directory

--- Regalo/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("openBtn").addEventListener("click", function () {
-  document.getElementById("envelope").style.display = "none";
-  this.style.display = "none";
-  document.getElementById("mensaje").style.display = "block";
-});
